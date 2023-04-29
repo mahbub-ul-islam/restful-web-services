f9a3a575f8605d985ecf9d9bc9c7cb5a51046892
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-29 13:40:27 +0600

    add static filtring. closes #6
    remove fields from all exposure with @JsonIgore

---
+++ src/main/java/com/learning/restfulwebservices/filter/SomeBean.java
@@ -0,0 +1,20 @@
+package com.learning.restfulwebservices.filter;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SomeBean {
+
+    private String field1;
+    private String field2;
+
+    @JsonIgnore
+    private String field3;
+}
