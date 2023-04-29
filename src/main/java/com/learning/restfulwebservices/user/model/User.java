36ceb912a1af203b4d21a540bae9195b9616b701
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-29 16:11:55 +0600

    add configuration for h2 db. closes #10

--- src/main/java/com/learning/restfulwebservices/user/model/User.java
+++ src/main/java/com/learning/restfulwebservices/user/model/User.java
@@ -3,6 +3,10 @@ package com.learning.restfulwebservices.user.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -12,8 +16,11 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Size(min = 2, message = "Name Should Have At Least 2 Character.")
