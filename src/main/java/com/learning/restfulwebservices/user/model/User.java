7e1b179d0b729a1d4772fa9dcfcf64f2cea74d44
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-29 13:30:06 +0600

    removing field from exposure. closes #6

--- src/main/java/com/learning/restfulwebservices/user/model/User.java
+++ src/main/java/com/learning/restfulwebservices/user/model/User.java
@@ -1,5 +1,6 @@
 package com.learning.restfulwebservices.user.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.validation.constraints.Past;
@@ -18,6 +19,7 @@ public class User {
     @Size(min = 2, message = "Name Should Have At Least 2 Character.")
     private String name;
 
+    @JsonIgnore
     @Past(message = "Birthdate Should be in past.")
     private Date birthDate;
 }
